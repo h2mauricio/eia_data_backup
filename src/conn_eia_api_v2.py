85914f014751811fec58f883d39fac949e133b14
mmh54 <mmh54@duke.edu>
2025-02-10 13:08:09 -0500

    feat: add request monthly elect prices

--- src/conn_eia_api_v2.py
+++ src/conn_eia_api_v2.py
@@ -14,12 +14,15 @@ hr_demand_path = Path(cwd).joinpath('..', 'data', 'operations_day_hour', 'hourly
 hr_demand_bytech_path = Path(cwd).joinpath('..', 'data', 'operations_day_hour', 'hourly_demand_bytech')
 hr_demand_bysubregion = Path(cwd).joinpath('..', 'data', 'operations_day_hour', 'hourly_demand_bysubregion')
 hr_interchange_path = Path(cwd).joinpath('..', 'data', 'operations_day_hour', 'hourly_interchange')
+month_elect_sales_path = Path(cwd).joinpath('..', 'data', 'operations_month', 'monthly_elect_sales')
 
 #create folders if they dont exist
 hr_demand_path.mkdir(parents=True, exist_ok=True)
 hr_demand_bytech_path.mkdir(parents=True, exist_ok=True)
 hr_demand_bysubregion.mkdir(parents=True, exist_ok=True)
 hr_interchange_path.mkdir(parents=True, exist_ok=True)
+month_elect_sales_path.mkdir(parents=True, exist_ok=True)
+
 
 api_key = '097E0917746D669FC846A22990D6F9CB'
 #api_key = '577ljs0wPebQR1SJ6vZRmXOdF1AWpZZEvsPWQrZH' #Matthew's API key
@@ -137,6 +140,117 @@ def req_day_hourly_power_ops(api_key: str, day_dt:dt.datetime, type_data:str) ->
         df_eia = df_eia.loc[df_eia['period'].dt.date == day_dt]
     return df_eia
 
+#%%
+def req_eia_month_elect_sales(api_url: str, api_key: str, year: int, month: int, offset:int=0, pause_time:float=0.3) -> pd.DataFrame:
+    """
+    Request electricity sales data for a specific year to the EIA API V2
+
+    Parameters
+    ----------
+    api_key : str
+        API key to access the EIA API. Can be requested at https://www.eia.gov/opendata/register.php
+    year : int
+        Year to request the data.
+
+    Returns
+    -------
+    dataframe
+        A dataframe with the requested data
+    """
+    
+    data = {}
+    params = {
+        "api_key": api_key,
+        "frequency": "monthly", # 'hourly' for UTC or 'local-hourly'
+        'data[0]':'customers',
+        'data[1]':'price',
+        'data[2]':'revenue',
+        'data[3]':'sales',
+        'offset':str(offset),
+        'length':'5000',
+        'sort[0][direction]':'asc',
+        'sort[0][column]':'period',
+        'start':str(year) + '-' + str(month).zfill(2),
+        'end':str(year) + '-' + str(month).zfill(2)}
+
+    try:
+        (r := requests.get(api_url, params=params)).raise_for_status()
+        print(f'Period: {str(year) + '-' + str(month).zfill(2)}')
+        data = r.json()['response']
+    except Exception as e:
+        print(f'Data acquisition failed due to {e}')
+    #This pause is sometimes needed to avoid a 429 error from the API
+    sleep(pause_time)
+    return data
+        
+def req_eia_year_elect_sales(year: int) -> pd.DataFrame:
+    """
+    Request electricity sales data for a specific year to the EIA API V2
+
+    Parameters
+    ----------
+    api_key : str
+        API key to access the EIA API. Can be requested at https://www.eia.gov/opendata/register.php
+    year : int
+        Year to request the data.
+
+    Returns
+    -------
+    dataframe
+        A dataframe with the requested data
+    """
+    url = 'https://api.eia.gov/v2/electricity/retail-sales/data/?'
+    
+    df_eia = pd.DataFrame()
+           
+    print("--------------------------------------------")
+    #print(f'Request {type_data} for year: {year}')
+    # TODO: This method could be more efficient. EIA's API limits its data 
+    # returns the first 5,000 rows
+    offset = 0
+    ini_month = 1
+    
+    ini_month_dt= dt.datetime(year, ini_month, 1)
+    print(f'Requesting data for year: {ini_month_dt.year}')
+    
+    for month in range(1, 13):
+        while True:
+            json_resp = req_eia_month_elect_sales(api_url= url,
+                                                    api_key= api_key,
+                                                    year= ini_month_dt.year,
+                                                    month= month,
+                                                    offset = offset)
+            
+            print(json_resp)
+            df_resp = pd.json_normalize(json_resp, record_path =['data'])
+            #check id df_resp is empty
+            if not df_resp.empty:
+                df_eia = pd.concat([df_eia, df_resp], axis=0)
+                df_eia['period'] = pd.to_datetime(df_eia['period'], format='%Y-%m')
+            else:
+                print('Empty response for day: ', day)
+                break    
+            
+            if len(df_resp) == 5000:
+                offset += 5000
+                continue
+            else:
+                break
+    # if df_eia is not empty, convert period to datetime
+    if not df_eia.empty:
+        df_eia['period'] = pd.to_datetime(df_eia['period'], utc=True)
+        #df_eia = df_eia.loc[df_eia['period'].dt.date == day_dt]
+    return df_eia
+        
+for year in range(2001, 2025):        
+    df_elect_sales = req_eia_year_elect_sales(year)
+    
+    #Save data to csv
+    file_pathname = month_elect_sales_path.joinpath(f'{year}_month_elect_sales.csv')
+    print(f"Saving monthly electricity sales data for {year} in CSV")
+    df_elect_sales.to_csv(file_pathname, index=False)
+    sleep(0.3)
+        
 #%%
 """## Request Demand, Net Generation, Forecasted Demand, and Interchange Data"""
 ini_day = '2025-02-01'
